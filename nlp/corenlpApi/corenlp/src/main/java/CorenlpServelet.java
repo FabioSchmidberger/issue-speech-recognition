42ec90590b60cb256849484a066bca0f340c8535
Fabio Schmidberger <fabio@voize.de>
2020-09-27 23:14:22 +0200

    update to maven project

--- nlp/corenlpApi/corenlp/src/main/java/CorenlpServelet.java
+++ nlp/corenlpApi/corenlp/src/main/java/CorenlpServelet.java
@@ -1,3 +1,5 @@
+import javax.servlet.http.HttpServlet;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -5,8 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class CorenlpServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
