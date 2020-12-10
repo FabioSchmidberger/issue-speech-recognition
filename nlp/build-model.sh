f5c4a91527a807395387db3099dd853fa1fa1c91
Fabio Schmidberger <fabio@voize.de>
2020-12-10 22:02:35 +0100

    nlp docker pipeline.

--- nlp/build-model.sh
+++ nlp/build-model.sh
@@ -3,17 +3,16 @@ BASEDIR=$(dirname $(readlink -f "$0"))
 repo_root_path=$(readlink -f "${BASEDIR}/..")
 (
   cd "$repo_root_path/nlp"
-  # use CORE_NLP_PATH variable if set (usually in CI), if not set use repo default path
+  # use CORE_NLP_PATH env variable if set (usually in CI), if not set use repo default path
   CORE_NLP_PATH="${CORE_NLP_PATH:-./corenlp/stanford-corenlp-full-2018-10-05/*}"
 
   # train model
   time java -Xmx2g -cp "$CORE_NLP_PATH" edu.stanford.nlp.ie.crf.CRFClassifier -prop ner.model.props
 
   MODEL_RESOURCE_PATH=./corenlpApi/models
-  # create model resource folder
-  #rm -r "$MODEL_RESOURCE_PATH"
+
   mkdir -p "$MODEL_RESOURCE_PATH"
-  # move model to resource folder so it can be packaged to jar
+  # move model to resource folder so it can be packaged to war
   mv ner.model.ser.gz "$MODEL_RESOURCE_PATH"
 )
 
